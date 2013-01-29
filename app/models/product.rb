31047e7ea1f9f9dfc7e9d997d26f6eb0a9a0d780
Jonathan Lee <joncslee@gmail.com>
2013-01-29 00:11:04 -0500

    get started on data fetch code

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,30 @@
 class Product < ActiveRecord::Base
   attr_accessible :amazon_description, :description, :name, :price, :url, :image_url
+
+  def self.fetch
+
+    # method: curate and collect ASIN,
+    # query web service using ASIN to
+    # obtain attributes and photos
+    # http://docs.aws.amazon.com/AWSECommerceService/latest/DG/CHAP_ApiReference.html
+
+    Amazon::Ecs.options = {
+      :associate_tag => 'grubg-20',
+      :AWS_access_key_id => 'AKIAJNFWYHUBM55U23AQ',       
+      :AWS_secret_key => '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'
+    }
+
+    res = Amazon::Ecs.item_search('gadget', :search_index => 'Kitchen', :response_group => 'OfferFull', :sort => 'salesrank')
+
+    res.items.each do |item|
+      # retrieve string value using XML path
+      item_attributes = item.get_element('ItemAttributes')
+
+      author = item_attributes.get('Author') 
+
+      puts "KKKKKK #{item_attributes}"
+      break
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130129040445) do
+ActiveRecord::Schema.define(:version => 20130129050133) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(:version => 20130129040445) do
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
     t.string   "image_url"
+    t.string   "asin"
   end
 
 end
