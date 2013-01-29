837c7b42ef6724b9576473e0fff05cfedd8e264c
Jonathan Lee <joncslee@gmail.com>
2013-01-29 00:25:16 -0500

    more mods to product lookup

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,14 @@
 class Product < ActiveRecord::Base
   attr_accessible :amazon_description, :description, :name, :price, :url, :image_url
 
-  def self.fetch
+  def self.fetch(asin)
 
     # method: curate and collect ASIN,
     # query web service using ASIN to
     # obtain attributes and photos
     # http://docs.aws.amazon.com/AWSECommerceService/latest/DG/CHAP_ApiReference.html
+    #
+    # self.item_lookup for ASIN lookups
 
     Amazon::Ecs.options = {
       :associate_tag => 'grubg-20',
@@ -14,7 +16,7 @@ class Product < ActiveRecord::Base
       :AWS_secret_key => 'rQ0IK0QdP8jW/RJev7dTmiXnfPbbJzTi/Ae9KED3'
     }
 
-    res = Amazon::Ecs.item_search('gadget', :search_index => 'Kitchen', :response_group => 'OfferFull', :sort => 'salesrank')
+    res = Amazon::Ecs.item_search('bake', :search_index => 'Kitchen', :response_group => 'Small', :sort => 'salesrank')
 
     res.items.each do |item|
       # retrieve string value using XML path
